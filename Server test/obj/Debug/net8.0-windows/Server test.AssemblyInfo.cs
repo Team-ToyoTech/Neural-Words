c8019fcb2e82d74415aa405cbd468bbd73c1139d
DH.L <meozigoon@gmail.com>
2025-04-27 13:31:35 +0900

    v0.0.2-a
    Server.cs: comment edited

--- Server test/obj/Debug/net8.0-windows/Server test.AssemblyInfo.cs
+++ Server test/obj/Debug/net8.0-windows/Server test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e3c8cbdadca3fbc6220b34687ad32a62a4c9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d348f3743ead3c5394232762077c9cf50a0f1d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
